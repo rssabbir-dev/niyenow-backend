9336e25f6c130b6a0bb4a5e0111f325350395818
rssabbir <sabbirhossainrahad@gmail.com>
2022-12-26 02:34:36 +0600

    add verifyJWT, add verifyAuthorization

--- index.js
+++ index.js
@@ -21,19 +21,44 @@ const client = new MongoClient(uri, {
 	serverApi: ServerApiVersion.v1,
 });
 
+const verifyJWT = (req, res, next) => {
+	const authHead = req.headers.authorization;
+	if (!authHead) {
+		return res.status(401).send({ code: 401, message: 'Access Denied' });
+	}
+	const token = authHead.split(' ')[1];
+	jwt.verify(token, process.env.JWT_SECRET_TOKEN, (err, decoded) => {
+		if (err) {
+			return res
+				.status(403)
+				.send({ code: 403, message: 'Access Forbidden' });
+		}
+		req.decoded = decoded;
+	});
+	next();
+};
+
+const verifyAuthorization = (req, res, uid) => {
+	const decoded = req.decoded;
+	if (uid !== decoded.uid) {
+		res.status(403).send({ code: 403, message: 'Access Forbidden' });
+		return false;
+	}
+	return true;
+};
+
 const run = async () => {
 	try {
 		const database = client.db('niyenowDB');
 		const productCollection = database.collection('products');
 		const userCollection = database.collection('users');
 
-        //GET JWT TOKEN
-        app.get('/jwt', (req, res) => {
-            const uid = req.query.uid;
-            const token = jwt.sign({ uid }, process.env.JWT_SECRET_TOKEN);
-            res.send(token)
-            console.log(token);
-        })
+		//GET JWT TOKEN
+		app.get('/jwt', (req, res) => {
+			const uid = req.query.uid;
+			const token = jwt.sign({ uid }, process.env.JWT_SECRET_TOKEN);
+			res.send({ token });
+		});
 		//product operation
 		app.get('/products', async (req, res) => {
 			const query = {};
@@ -46,7 +71,12 @@ const run = async () => {
 			const product = await productCollection.findOne(query);
 			res.send(product);
 		});
-		app.post('/product', async (req, res) => {
+		app.post('/product',verifyJWT, async (req, res) => {
+			const uid = req.query.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
 			const product = req.body;
 			const result = await productCollection.insertOne(product);
 			res.send(result);
@@ -59,10 +89,10 @@ const run = async () => {
 			const query = { uid: user.uid };
 			const exit = await userCollection.findOne(query);
 			if (exit) {
-                return;
-            }
-            const result = await userCollection.insertOne(user);
-            res.send(result)
+				return;
+			}
+			const result = await userCollection.insertOne(user);
+			res.send(result);
 		});
 
 		//Admin
@@ -70,14 +100,18 @@ const run = async () => {
 			const uid = req.query.uid;
 			const query = { uid: uid };
 			const user = await userCollection.findOne(query);
-            res.send({ role: user.role });
-        });
-        app.get('/admin-products/:uid', async (req, res) => {
-            const uid = req.params.uid;
-            const query = { 'seller_info.seller_uid': uid };
-            const products = await productCollection.find(query).toArray()
-            res.send(products)
-        })
+			res.send({ role: user.role });
+		});
+		app.get('/admin-products/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
+			const query = { 'seller_info.seller_uid': uid };
+			const products = await productCollection.find(query).toArray();
+			res.send(products);
+		});
 	} finally {
 	}
 };
