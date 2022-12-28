60e08c30363af6680d1036c399a77b9f6ab008b0
rssabbir <sabbirhossainrahad@gmail.com>
2022-12-28 16:43:33 +0600

    implement order system and payment system

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 //Middleware
 app.use(cors());
@@ -52,11 +53,15 @@ const run = async () => {
 		const database = client.db('niyenowDB');
 		const productCollection = database.collection('products');
 		const userCollection = database.collection('users');
+		const categoryCollection = database.collection('categories');
+		const cartCollection = database.collection('carts');
+		const orderCollection = database.collection('orders');
+		const paymentCollection = database.collection('payments')
 
 		//VerifyAdmin
 		const verifyAdmin = async (req, res, next) => {
 			const decoded = req.decoded;
-			const query = { uid: decoded.uid };
+			const query = { uid: decoded?.uid };
 			const user = await userCollection.findOne(query);
 			if (user.role !== 'admin') {
 				return res
@@ -105,6 +110,8 @@ const run = async () => {
 			}
 			const result = await userCollection.insertOne(user);
 			res.send(result);
+			console.log(user);
+			console.log(result);
 		});
 
 		//Admin
@@ -131,24 +138,16 @@ const run = async () => {
 		);
 
 		//cart
-		app.patch('/add-to-cart/:uid', verifyJWT, async (req, res) => {
+		app.post('/add-to-cart/:uid', verifyJWT, async (req, res) => {
 			const uid = req.params.uid;
 			const order = req.body;
 			const valid = verifyAuthorization(req, res, uid);
 			if (!valid) {
 				return;
 			}
-			const filter = { uid };
-			const option = { upsert: true };
-			const updatedDoc = {
-				$push: {
-					cart: order,
-				},
-			};
-			const result = await userCollection.updateOne(filter, updatedDoc);
-			console.log(result);
-			res.send(result)
-		})
+			const result = await cartCollection.insertOne(order);
+			res.send(result);
+		});
 		app.get('/get-cart/:uid', verifyJWT, async (req, res) => {
 			const uid = req.params.uid;
 			const valid = verifyAuthorization(req, res, uid);
@@ -156,8 +155,130 @@ const run = async () => {
 				return;
 			}
 			const query = { uid };
-			const user = await userCollection.findOne(query);
-			res.send({ cart: user.cart });
+			const cart = await cartCollection.find(query).toArray();
+			res.send(cart);
+		});
+		app.delete('/delete-cart/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const id = req.query.id;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
+			const query = { _id: ObjectId(id) };
+			const result = await cartCollection.deleteOne(query);
+			res.send(result);
+		});
+
+		//Categories
+		app.get('/categories', async (req, res) => {
+			const query = {};
+			const categories = await categoryCollection.find(query).toArray();
+			res.send(categories);
+		});
+		app.post('/categories', verifyJWT, verifyAdmin, async (req, res) => {
+			const uid = req.query.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
+			const category = req.body;
+			const result = await categoryCollection.insertOne(category);
+			res.send(result);
+			console.log('gg');
+		});
+
+		//Customer
+		app.get('/customers/:uid', verifyJWT, verifyAdmin, async (req, res) => {
+			const uid = req.params.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
+			const query = { role: 'customer' };
+			const customers = await userCollection.find(query).toArray();
+			console.log(customers);
+			res.send(customers);
+		});
+
+		//Confirm Order
+		app.post('/confirm-order/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
+			const order = req.body;
+			const result = await orderCollection.insertOne(order);
+			res.send(result);
+			const deleteCart = await cartCollection.deleteMany({ uid });
+		});
+		//get orders
+		app.get('/get-orders/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return;
+			}
+			const orders = await orderCollection.findOne({ customer_uid: uid });
+			res.send(orders);
+		});
+
+		//Create Payment Intent
+		app.post('/create-payment-intent/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const id = req.query.id;
+			const valid = verifyAuthorization(req, res, uid)
+			if (!valid) {
+				return
+			}
+			const query = { _id: ObjectId(id) };
+			const order = await orderCollection.findOne(query);
+			const price = order.subTotal;
+			const paymentIntent = await stripe.paymentIntents.create({
+				amount: price,
+				currency: 'usd',
+				payment_method_types: ['card'],
+			});
+			res.send({ clientSecret: paymentIntent.client_secret });
+		});
+
+		//save payment data to database
+		app.post('/payments/:uid', verifyJWT, async (req, res) => {
+			const uid = req.params.uid;
+			const valid = verifyAuthorization(req, res, uid);
+			if (!valid) {
+				return
+			}
+			const payment = req.body;
+			const result = await paymentCollection.insertOne(payment);
+			res.send(result);
+			//set order status true after payment done
+			const orderQuery = { _id: ObjectId(payment.orderId) };
+			const option = { upsert: true };
+			const orderUpdatedDoc = {
+				$set: {
+					payment_status: true,
+				},
+			};
+			const orderResult = await orderCollection.updateOne(
+				orderQuery,
+				orderUpdatedDoc,
+				option
+			);
+			//set order status true and promote status false after payment done
+			// const productQuery = { _id: ObjectId(payment.product_id) };
+			// const productUpdatedDoc = {
+			// 	$set: {
+			// 		order_status: true,
+			// 		promote: false,
+			// 	},
+			// };
+			// const productResult = await productCollection.updateOne(
+			// 	productQuery,
+			// 	productUpdatedDoc,
+			// 	option
+			// );
 		});
 	} finally {
 	}
